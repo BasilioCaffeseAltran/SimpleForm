1bd8b0e4a3527ae2bce12fb98ebff8e3649d99df
BasilioCaffeseAltran <41633823+BasilioCaffeseAltran@users.noreply.github.com>
2018-07-24 15:29:50 +0200

    Update Program.cs

--- TesteoCns/TesteoCns/Program.cs
+++ TesteoCns/TesteoCns/Program.cs
@@ -6,7 +6,7 @@ namespace TesteoCns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World???!!! Are you crazy ");
         }
     }
 }
