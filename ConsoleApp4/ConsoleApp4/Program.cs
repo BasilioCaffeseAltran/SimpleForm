15c7a9dfc9dfa229231d2dc9bf93d0fc3efc3be9
caffese <caffese@CAFFESE3>
2018-07-24 15:31:15 +0200

    Agregue algo

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Console = System.Console;
 
 namespace ConsoleApp4
 {
@@ -10,6 +11,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Esto es una bibaka");
         }
     }
 }
